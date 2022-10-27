c37699e08dab3cb086b8000be11f06c39a201860
Дарья <dnovogodniaya@ya.ru>
2022-10-27 17:04:07 +0300

    task

---
+++ src/course.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class course {
+    public static void main(String[] args) {
+        byte [] massiv=new byte[3];
+        massiv[0] = 1;
+        massiv[1] = 2;
+        massiv[2] = 3;
+        byte  a=0;
+        byte n=0;
+        byte [] masiv=new byte[3];
+        for (int i=massiv.length-1;i>=0;i--){
+            a=massiv[i];
+            masiv[n]=massiv[i];
+            n+=1;
+            //System.out.println(masiv[i]);
+        }
+        //masiv[0]=3;
+        System.out.println(Arrays.toString(masiv));
+
+    }
+}
